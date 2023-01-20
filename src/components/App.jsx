0ab58eead38f556842bd194f0fb008f1f85146fe
Natalya Karpova <knataly0513@gmail.com>
2023-01-20 12:15:41 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,11 @@ import { Filter } from "./Filter";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const defaultContacts = [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
@@ -25,7 +25,7 @@ export const App = () => {
   const [filter, setFilter] = useState('');
   const changeFilter = e => {
     setFilter(e.target.value);
-  }
+  };
 
   const formSubmitHandler = ({ name, number }) => {
     const currentContact = { name: name, id: shortid.generate(), number: number }
@@ -34,8 +34,8 @@ export const App = () => {
       Notify.failure(`${currentContact.name} is allready in contacts.`);
       return;
     }
-      setContacts(prev => [...prev, currentContact]
-      );
+    setContacts(prev => [...prev, currentContact]
+    );
   };
 
   const normalizedFilter = filter.toLowerCase();
@@ -43,120 +43,32 @@ export const App = () => {
   
   const deleteContact = id => {
     setContacts(contacts.filter(c => c.id !== id));
-  }
+  };
 
   return (
-      <Box
-        bg="light"
-        color="text"
-        width="containerWidth"
-        position="relative"
-        p={6}
-        my={0}
-        mx="auto"
-        boxShadow="containerShadow"
-        borderRadius="normal"
-        overflow="hidden"
-        fontFamily="heading"
-      >
-        <h1>Phonebook</h1>
+    <Box
+      bg="light"
+      color="text"
+      width="containerWidth"
+      position="relative"
+      p={6}
+      my={0}
+      mx="auto"
+      boxShadow="containerShadow"
+      borderRadius="normal"
+      overflow="hidden"
+      fontFamily="heading"
+    >
+      <h1>Phonebook</h1>
       <ContactForm
         onSubmit={formSubmitHandler} />
-        <h2>Contacts</h2>
+      <h2>Contacts</h2>
       <Filter
         value={filter}
         onChange={changeFilter} />
       <ContactList
         contacts={visibleContacts}
         onDeleteContact={deleteContact} />
-      </Box>
-    ); 
-}
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   }
-
-//   //Виполняется каждій раз при первой загрузке страниці
-//   //Можна повесить слушателя на window
-//   componentDidMount() {
-//     const contactsFromLS = JSON.parse(localStorage.getItem('contacts'));
-//     if (contactsFromLS) {
-//       this.setState({ contacts: contactsFromLS });
-//     }
-//   }
-
-//   //Віполняется каждій раз при обновлении стейта до рендера
-//   componentDidUpdate(_, prevState) {
-
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   changeFilter = e => {
-//     this.setState({ filter: e.currentTarget.value });
-//   }
-
-//   formSubmitHandler = ({ name, number }) => {
-//     const currentContact = { name: name, id: shortid.generate(), number: number }
-//     const contactDublicate = this.state.contacts.find(c => c.name === currentContact.name);
-//     if (contactDublicate) {
-//       Notify.failure(`${currentContact.name} is allready in contacts.`);
-//       return;
-//     }
-//       this.setState(prevState => ({
-//         contacts: [currentContact, ...prevState.contacts]
-//       }));
-//   };
-
-//   deleteContact = (contactId) => {
-//     console.log('contactId:', contactId);
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-//     }))
-//   }
-
-//   getVisibleContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-    
-//     return contacts.filter(n => n.name.toLowerCase().includes(normalizedFilter));
-//   }
-  
-
-//   render() {
-//     console.log('App render');
-//     const { filter } = this.state;
-//     const visibleContacts = this.getVisibleContacts();
-    
-//     return (
-//       <Box
-//         bg="light"
-//         color="text"
-//         width="containerWidth"
-//         position="relative"
-//         p={6}
-//         my={0}
-//         mx="auto"
-//         boxShadow="containerShadow"
-//         borderRadius="normal"
-//         overflow="hidden"
-//         fontFamily="heading"
-//       >
-//         <h1>Phonebook</h1>
-//         <ContactForm onSubmit={this.formSubmitHandler}/>
-//         <h2>Contacts</h2>
-//         <Filter value={filter} onChange={this.changeFilter}/>
-//         <ContactList contacts={visibleContacts} onDeleteContact={this.deleteContact}/>
-//       </Box>
-//     ); 
-//   }
-// }
+    </Box>
+  );
+};
